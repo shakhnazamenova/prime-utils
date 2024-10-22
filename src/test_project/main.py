6bd4363fbf9b63d8300c7891c1f107b3c9a31a7c
shai <shakhnazamenova@gmail.com>
2024-10-23 01:00:22 +0500

    Обновлен main.py

--- src/test_project/main.py
+++ src/test_project/main.py
@@ -1,9 +1,8 @@
 from pipeline import generate_and_process_primes
 
 def main():
-    test_list = [1, 2, 6, 24]
-    result = checksum(test_list)
-    print(f"Контрольная сумма для {test_list}: {result}")
+    result = generate_and_process_primes()
+    print(f"Контрольная сумма первых 1000 простых чисел: {result}")
 
 if __name__ == "__main__":
     main()
